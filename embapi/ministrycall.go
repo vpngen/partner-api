b2cb1a1d469f9544004a2b97b65d715d3631b675
Ingmund Olsson <ingmund@proton.me>
2023-09-15 17:24:28 +0300

    debug

--- embapi/ministrycall.go
+++ embapi/ministrycall.go
@@ -7,20 +7,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"math/rand"
 	"net/http/httputil"
 	"net/netip"
 	"os"
 	"strings"
-	"time"
 
-	"github.com/vpngen/keydesk/kdlib"
 	"github.com/vpngen/ministry"
-	ilib "github.com/vpngen/partner-api/internal/kdlib"
-	"github.com/vpngen/wordsgens/namesgenerator"
-	"github.com/vpngen/wordsgens/seedgenerator"
 	"golang.org/x/crypto/ssh"
-	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 )
 
 const (
@@ -157,69 +150,85 @@ func genGrants(dgst string, conf *ssh.ClientConfig, addr netip.AddrPort) (*grant
 		return nil, fmt.Errorf("json unmarshal: %w", err)
 	}
 
-	// ------------------------
+	if wgconf.Configs.WireguardConfig == nil ||
+		wgconf.Configs.WireguardConfig.FileContent == nil ||
+		wgconf.Configs.WireguardConfig.FileName == nil ||
+		wgconf.Configs.WireguardConfig.TonnelName == nil {
+		return nil, fmt.Errorf("wgconf read: %w", err)
+	}
 
 	pkg := &grantPkg{}
 
-	fullname, person, err := namesgenerator.PhysicsAwardeeShort()
-	if err != nil {
-		return nil, fmt.Errorf("physics gen: %w", err)
-	}
-
-	pkg.fullname = testPrefix + fullname
-	pkg.person = person.Name
-	pkg.desc = person.Desc
-	pkg.wiki = person.URL
-
-	pkg.mnemo, _, _, err = seedgenerator.Seed(seedgenerator.ENT64, fakeSeedPrefix)
-	if err != nil {
-		return nil, fmt.Errorf("gen seed6: %w", err)
-	}
-
-	pkg.keydesk = kdlib.RandomAddrIPv6(netip.MustParsePrefix(fakeKeydeskPrefix)).String()
-
-	numbered := fmt.Sprintf("%03d %s", rand.Int31n(256), fullname)
-	pkg.filename = ilib.SanitizeFilename(numbered) + ".conf"
-
-	wgkey, err := wgtypes.GenerateKey()
-	if err != nil {
-		return nil, fmt.Errorf("gen wg psk: %w", err)
-	}
-
-	wgpriv, err := wgtypes.GeneratePrivateKey()
-	if err != nil {
-		return nil, fmt.Errorf("gen wg psk: %w", err)
-	}
-
-	wgpub := wgpriv.PublicKey()
-
-	tmpl := `[Interface]
-Address = %s
-PrivateKey = %s
-DNS = %s
-
-[Peer]
-Endpoint = %s:51820
-PublicKey = %s
-PresharedKey = %s
-AllowedIPs = 0.0.0.0/0,::/0
-`
-
-	ipv4 := kdlib.RandomAddrIPv4(netip.MustParsePrefix(fakeCGNAT))
-	ipv6 := kdlib.RandomAddrIPv6(netip.MustParsePrefix(fakeULA))
-	ep := kdlib.RandomAddrIPv4(netip.MustParsePrefix(fakeEndpointNet))
-
-	pkg.wgconf = fmt.Sprintf(
-		tmpl,
-		netip.PrefixFrom(ipv4, 32).String()+","+netip.PrefixFrom(ipv6, 128).String(),
-		base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString(wgpriv[:]),
-		ipv4.String()+","+ipv6.String(),
-		ep.String(),
-		base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString(wgpub[:]),
-		base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString(wgkey[:]),
-	)
-
-	time.Sleep(15 * time.Second)
+	pkg.fullname = wgconf.Name
+	pkg.person = wgconf.Person.Name
+	pkg.desc = wgconf.Person.Desc
+	pkg.wiki = wgconf.Person.URL
+	pkg.mnemo = wgconf.Mnemo
+	pkg.keydesk = wgconf.KeydeskIPv6.String()
+	pkg.filename = *wgconf.Configs.WireguardConfig.FileName
+	pkg.wgconf = *wgconf.Configs.WireguardConfig.FileContent
 
 	return pkg, nil
+
+	// ------------------------
+
+	/*fullname, person, err := namesgenerator.PhysicsAwardeeShort()
+		if err != nil {
+			return nil, fmt.Errorf("physics gen: %w", err)
+		}
+
+		pkg.fullname = testPrefix + fullname
+		pkg.person = person.Name
+		pkg.desc = person.Desc
+		pkg.wiki = person.URL
+
+		pkg.mnemo, _, _, err = seedgenerator.Seed(seedgenerator.ENT64, fakeSeedPrefix)
+		if err != nil {
+			return nil, fmt.Errorf("gen seed6: %w", err)
+		}
+
+		pkg.keydesk = kdlib.RandomAddrIPv6(netip.MustParsePrefix(fakeKeydeskPrefix)).String()
+
+		numbered := fmt.Sprintf("%03d %s", rand.Int31n(256), fullname)
+		pkg.filename = ilib.SanitizeFilename(numbered) + ".conf"
+
+		wgkey, err := wgtypes.GenerateKey()
+		if err != nil {
+			return nil, fmt.Errorf("gen wg psk: %w", err)
+		}
+
+		wgpriv, err := wgtypes.GeneratePrivateKey()
+		if err != nil {
+			return nil, fmt.Errorf("gen wg psk: %w", err)
+		}
+
+		wgpub := wgpriv.PublicKey()
+
+		tmpl := `[Interface]
+	Address = %s
+	PrivateKey = %s
+	DNS = %s
+
+	[Peer]
+	Endpoint = %s:51820
+	PublicKey = %s
+	PresharedKey = %s
+	AllowedIPs = 0.0.0.0/0,::/0
+	`
+
+		ipv4 := kdlib.RandomAddrIPv4(netip.MustParsePrefix(fakeCGNAT))
+		ipv6 := kdlib.RandomAddrIPv6(netip.MustParsePrefix(fakeULA))
+		ep := kdlib.RandomAddrIPv4(netip.MustParsePrefix(fakeEndpointNet))
+
+		pkg.wgconf = fmt.Sprintf(
+			tmpl,
+			netip.PrefixFrom(ipv4, 32).String()+","+netip.PrefixFrom(ipv6, 128).String(),
+			base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString(wgpriv[:]),
+			ipv4.String()+","+ipv6.String(),
+			ep.String(),
+			base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString(wgpub[:]),
+			base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString(wgkey[:]),
+		)
+
+		return pkg, nil*/
 }
