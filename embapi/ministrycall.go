575f957c5fa1abe6e89fe2e643ebfecbb1d93ab5
Ingmund Olsson <ingmund@proton.me>
2023-09-15 18:08:39 +0300

    debug

--- embapi/ministrycall.go
+++ embapi/ministrycall.go
@@ -3,16 +3,23 @@ package embapi
 import (
 	"bufio"
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http/httputil"
 	"net/netip"
 	"os"
 	"strings"
 
+	"github.com/vpngen/keydesk/kdlib"
 	"github.com/vpngen/ministry"
+	ilib "github.com/vpngen/partner-api/internal/kdlib"
+	"github.com/vpngen/wordsgens/namesgenerator"
+	"github.com/vpngen/wordsgens/seedgenerator"
 	"golang.org/x/crypto/ssh"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 )
 
 const (
@@ -96,115 +103,42 @@ func callMinistry(dgst string, conf *ssh.ClientConfig, addr netip.AddrPort) (*gr
 	return pkg, nil
 }
 
-func genGrants(dgst string, conf *ssh.ClientConfig, addr netip.AddrPort) (*grantPkg, error) {
-	// base64name := base64.StdEncoding.EncodeToString([]byte("Веселый Кеттерле"))
-	// base64words := base64.StdEncoding.EncodeToString([]byte("несчастный подводный бывать победа долг дядя"))
-	// cmd := fmt.Sprintf("restorebrigadier -ch -j %s %s", base64name, base64words)
-	cmd := fmt.Sprintf("createbrigade -ch -j %s", dgst)
-
-	fmt.Fprintf(os.Stderr, "%s#%s -> %s\n", conf.User, addr, cmd)
-
-	client, err := ssh.Dial("tcp", addr.String(), conf)
-	if err != nil {
-		return nil, fmt.Errorf("ssh dial: %w", err)
-	}
-	defer client.Close()
-
-	session, err := client.NewSession()
-	if err != nil {
-		return nil, fmt.Errorf("ssh session: %w", err)
-	}
-	defer session.Close()
-
-	var b, e bytes.Buffer
-
-	session.Stdout = &b
-	session.Stderr = &e
-
-	LogTag := "embapi"
-	defer func() {
-		switch errstr := e.String(); errstr {
-		case "":
-			fmt.Fprintf(os.Stderr, "%s: SSH Session StdErr: empty\n", LogTag)
-		default:
-			fmt.Fprintf(os.Stderr, "%s: SSH Session StdErr:\n", LogTag)
-			for _, line := range strings.Split(errstr, "\n") {
-				fmt.Fprintf(os.Stderr, "%s: | %s\n", LogTag, line)
-			}
-		}
-	}()
-
-	if err := session.Run(cmd); err != nil {
-		return nil, fmt.Errorf("ssh run: %w", err)
-	}
-
-	r := bufio.NewReader(httputil.NewChunkedReader(&b))
-
-	payload, err := io.ReadAll(r)
-	if err != nil {
-		return nil, fmt.Errorf("chunk read: %w", err)
-	}
-
-	wgconf := &ministry.Answer{}
-	if err := json.Unmarshal(payload, &wgconf); err != nil {
-		return nil, fmt.Errorf("json unmarshal: %w", err)
-	}
-
-	if wgconf.Configs.WireguardConfig == nil ||
-		wgconf.Configs.WireguardConfig.FileContent == nil ||
-		wgconf.Configs.WireguardConfig.FileName == nil ||
-		wgconf.Configs.WireguardConfig.TonnelName == nil {
-		return nil, fmt.Errorf("wgconf read: %w", err)
-	}
-
+func genGrants() (*grantPkg, error) {
 	pkg := &grantPkg{}
 
-	pkg.fullname = wgconf.Name
-	pkg.person = wgconf.Person.Name
-	pkg.desc = wgconf.Person.Desc
-	pkg.wiki = wgconf.Person.URL
-	pkg.mnemo = wgconf.Mnemo
-	pkg.keydesk = wgconf.KeydeskIPv6.String()
-	pkg.filename = *wgconf.Configs.WireguardConfig.FileName
-	pkg.wgconf = *wgconf.Configs.WireguardConfig.FileContent
+	fullname, person, err := namesgenerator.PhysicsAwardeeShort()
+	if err != nil {
+		return nil, fmt.Errorf("physics gen: %w", err)
+	}
 
-	return pkg, nil
+	pkg.fullname = testPrefix + fullname
+	pkg.person = person.Name
+	pkg.desc = person.Desc
+	pkg.wiki = person.URL
 
-	// ------------------------
+	pkg.mnemo, _, _, err = seedgenerator.Seed(seedgenerator.ENT64, fakeSeedPrefix)
+	if err != nil {
+		return nil, fmt.Errorf("gen seed6: %w", err)
+	}
 
-	/*fullname, person, err := namesgenerator.PhysicsAwardeeShort()
-		if err != nil {
-			return nil, fmt.Errorf("physics gen: %w", err)
-		}
+	pkg.keydesk = kdlib.RandomAddrIPv6(netip.MustParsePrefix(fakeKeydeskPrefix)).String()
 
-		pkg.fullname = testPrefix + fullname
-		pkg.person = person.Name
-		pkg.desc = person.Desc
-		pkg.wiki = person.URL
+	numbered := fmt.Sprintf("%03d %s", rand.Int31n(256), fullname)
+	pkg.filename = ilib.SanitizeFilename(numbered) + ".conf"
 
-		pkg.mnemo, _, _, err = seedgenerator.Seed(seedgenerator.ENT64, fakeSeedPrefix)
-		if err != nil {
-			return nil, fmt.Errorf("gen seed6: %w", err)
-		}
+	wgkey, err := wgtypes.GenerateKey()
+	if err != nil {
+		return nil, fmt.Errorf("gen wg psk: %w", err)
+	}
 
-		pkg.keydesk = kdlib.RandomAddrIPv6(netip.MustParsePrefix(fakeKeydeskPrefix)).String()
+	wgpriv, err := wgtypes.GeneratePrivateKey()
+	if err != nil {
+		return nil, fmt.Errorf("gen wg psk: %w", err)
+	}
 
-		numbered := fmt.Sprintf("%03d %s", rand.Int31n(256), fullname)
-		pkg.filename = ilib.SanitizeFilename(numbered) + ".conf"
+	wgpub := wgpriv.PublicKey()
 
-		wgkey, err := wgtypes.GenerateKey()
-		if err != nil {
-			return nil, fmt.Errorf("gen wg psk: %w", err)
-		}
-
-		wgpriv, err := wgtypes.GeneratePrivateKey()
-		if err != nil {
-			return nil, fmt.Errorf("gen wg psk: %w", err)
-		}
-
-		wgpub := wgpriv.PublicKey()
-
-		tmpl := `[Interface]
+	tmpl := `[Interface]
 	Address = %s
 	PrivateKey = %s
 	DNS = %s
@@ -216,19 +150,19 @@ func genGrants(dgst string, conf *ssh.ClientConfig, addr netip.AddrPort) (*grant
 	AllowedIPs = 0.0.0.0/0,::/0
 	`
 
-		ipv4 := kdlib.RandomAddrIPv4(netip.MustParsePrefix(fakeCGNAT))
-		ipv6 := kdlib.RandomAddrIPv6(netip.MustParsePrefix(fakeULA))
-		ep := kdlib.RandomAddrIPv4(netip.MustParsePrefix(fakeEndpointNet))
+	ipv4 := kdlib.RandomAddrIPv4(netip.MustParsePrefix(fakeCGNAT))
+	ipv6 := kdlib.RandomAddrIPv6(netip.MustParsePrefix(fakeULA))
+	ep := kdlib.RandomAddrIPv4(netip.MustParsePrefix(fakeEndpointNet))
 
-		pkg.wgconf = fmt.Sprintf(
-			tmpl,
-			netip.PrefixFrom(ipv4, 32).String()+","+netip.PrefixFrom(ipv6, 128).String(),
-			base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString(wgpriv[:]),
-			ipv4.String()+","+ipv6.String(),
-			ep.String(),
-			base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString(wgpub[:]),
-			base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString(wgkey[:]),
-		)
+	pkg.wgconf = fmt.Sprintf(
+		tmpl,
+		netip.PrefixFrom(ipv4, 32).String()+","+netip.PrefixFrom(ipv6, 128).String(),
+		base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString(wgpriv[:]),
+		ipv4.String()+","+ipv6.String(),
+		ep.String(),
+		base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString(wgpub[:]),
+		base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString(wgkey[:]),
+	)
 
-		return pkg, nil*/
+	return pkg, nil
 }
